41cb993d06a1fc102ff65cd214a9498b73bfda68
Tejas Kulkarni <test@gmail.com>
2021-12-29 13:25:08 +0530

    Fixed new 1 bug

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Git Practice</h1>
       <p>This is -a tag used to directly stage all the changes made in the file</p>
+      <p>Fixed new bug in the file</p>
     </div>
   );
 }

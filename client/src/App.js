d477cc671d70ca792bfffb99e2725d5ecaca6356
Tejas Kulkarni <test@gmail.com>
2021-12-29 13:51:33 +0530

    Fixed /merege-conflict/ bug

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1>Git Practice</h1>
       <p>This is -a tag used to directly stage all the changes made in the file</p>
       <p>Fixed new bug in the file</p>
+      <p>Finally this bug is fixed "Merge Conflict"</p>
     </div>
   );
 }

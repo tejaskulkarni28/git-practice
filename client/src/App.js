46b03abd1d9d896c0e1ab231dee078fc307cce48
Tejas Kulkarni <test@gmail.com>
2021-12-29 13:36:07 +0530

    The emergency bug has been fixed

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>Git Practice</h1>
-      <p>This is -a tag used to directly stage all the changes made in the file</p>
+      <p>This is "-a" tag used to directly stage all the changes made in the file</p>
       <p>Fixed new bug in the file</p>
+      <p>This changes in "Emergency-fix-branch" is going to merege in master branch</p>
     </div>
   );
 }
